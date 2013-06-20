d14ee88c8bc7de03f4f7e00212541263249aa783
Javier Gamarra <nhpatt@gmail.com>
2013-06-20 16:18:20 +0200

    mapeo mal hecho pero funciona! :P

--- test/com/nhpatt/hibernate/expediente/ExpedienteTest.java
+++ test/com/nhpatt/hibernate/expediente/ExpedienteTest.java
@@ -9,7 +9,6 @@ import com.nhpatt.hibernate.utils.HibernateTest;
 
 public class ExpedienteTest extends HibernateTest {
 
-
 	@Test
 	public void insertarHistorialConExpedientes() {
 		Session session = getSession();
@@ -20,6 +19,26 @@ public class ExpedienteTest extends HibernateTest {
 		expediente.setHistorial(historial);
 		historial.getExpedientes().add(expediente);
 		session.save(historial);
+
+		session.getTransaction().commit();
+		session.close();
+
+		session = getSession();
+		Historial historialRecuperado = (Historial) session.get(
+				Historial.class, historial.getId());
+		Assert.assertFalse(historialRecuperado.getExpedientes().isEmpty());
+
+	}
+
+	@Test
+	public void insertarExpedienteConHistorialSinCruce() {
+		Session session = getSession();
+		session.beginTransaction();
+
+		Historial historial = new Historial();
+		Expediente expediente = new Expediente();
+		expediente.setHistorial(historial);
+		session.save(expediente);
 
 		session.getTransaction().commit();
 		session.close();
