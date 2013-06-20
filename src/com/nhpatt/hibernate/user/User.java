63c3a22f8de9d85a33e446285479bb545e5e8730
Javier Gamarra <nhpatt@gmail.com>
2013-06-20 08:03:34 +0200

    cambiar el nombre de una columna

--- src/com/nhpatt/hibernate/user/User.java
+++ src/com/nhpatt/hibernate/user/User.java
@@ -1,5 +1,6 @@
 package com.nhpatt.hibernate.user;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -8,6 +9,7 @@ public class User {
 
 	@Id
 	private Integer id;
+	@Column(name = "userName")
 	private String name;
 
 	public Integer getId() {
