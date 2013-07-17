b64b87aad9f8149e5b2745e1a7554da997361840
Javier Gamarra <nhpatt@gmail.com>
2013-07-17 10:20:40 +0200

    Persistent objects sync their state without a explicit save

--- test/com/nhpatt/hibernate/session/SessionTest.java
+++ test/com/nhpatt/hibernate/session/SessionTest.java
@@ -24,9 +24,39 @@ public class SessionTest extends HibernateTest {
 
 		session = getSession();
 		User userFromBD = (User) session.get(User.class, user.getId());
-		assertEquals("Lola",userFromBD.getName());
+		assertEquals("Lola", userFromBD.getName());
 		session.close();
 
+	}
+
+	@Test
+	public void loadAndChangeValueTest() {
+		User user = saveUser("Pepe", "Rayuela");
+
+		Session session = getSession();
+		session.beginTransaction();
+		User userToChange = (User) session.get(User.class, user.getId());
+		userToChange.setName("Lola");
+		session.getTransaction().commit();
+		session.close();
+
+		session = getSession();
+		User userFromBD = (User) session.get(User.class, user.getId());
+		assertEquals("Lola", userFromBD.getName());
+		session.close();
+
+	}
+
+	private User saveUser(String name, String surname) {
+		Session session = getSession();
+		session.beginTransaction();
+
+		User user = new User(name, surname);
+		session.save(user);
+
+		session.getTransaction().commit();
+		session.close();
+		return user;
 	}
 
 }
